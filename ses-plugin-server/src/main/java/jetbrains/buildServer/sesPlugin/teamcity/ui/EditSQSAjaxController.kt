3d2adfefcd576d7ebb2986b958c57632aa3a26bc
linfar <andrey.titov@jetbrains.com>
2017-10-18 11:20:54 +0300

    Init method

--- ses-plugin-server/src/main/java/jetbrains/buildServer/sesPlugin/teamcity/ui/EditSQSAjaxController.kt
+++ ses-plugin-server/src/main/java/jetbrains/buildServer/sesPlugin/teamcity/ui/EditSQSAjaxController.kt
@@ -6,13 +6,24 @@ import jetbrains.buildServer.controllers.BaseController
 import jetbrains.buildServer.sesPlugin.teamcity.ui.ajax.AjaxRequest
 import jetbrains.buildServer.sesPlugin.teamcity.ui.ajax.AjaxRequestData
 import jetbrains.buildServer.sesPlugin.teamcity.ui.ajax.AjaxRequestResult
+import jetbrains.buildServer.web.openapi.WebControllerManager
 import org.springframework.web.servlet.ModelAndView
 import org.springframework.web.servlet.view.json.MappingJackson2JsonView
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
-class EditSQSAjaxController(private val ajaxRequests: List<AjaxRequest>) : BaseController() {
+class EditSQSAjaxController(private val ajaxRequests: List<AjaxRequest>,
+                            private val webControllerManager: WebControllerManager) : BaseController() {
+
+    private val URL = "/admin/editSQSParams.html"
+
+    fun init() {
+        webControllerManager.registerController(URL, this)
+    }
+
     override fun doHandle(request: HttpServletRequest, response: HttpServletResponse): ModelAndView? {
+        // todo handle permissions
+
         val jsonString = request.getParameter("json") ?: return null
 
         val req = ObjectMapper().readValue<AjaxRequestData>(jsonString, AjaxRequestData::class.java)
