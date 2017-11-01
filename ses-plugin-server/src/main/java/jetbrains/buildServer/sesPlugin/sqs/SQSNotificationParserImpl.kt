f51f691f85fc4c31d389d93a5c88a001c88e0f50
linfar <andrey.titov@jetbrains.com>
2017-11-01 15:03:02 +0300

    Fix compilation; custom deserializer for AmazonSQSNotification to automatically parse embedded json object

--- ses-plugin-server/src/main/java/jetbrains/buildServer/sesPlugin/sqs/SQSNotificationParserImpl.kt
+++ ses-plugin-server/src/main/java/jetbrains/buildServer/sesPlugin/sqs/SQSNotificationParserImpl.kt
@@ -1,9 +1,16 @@
 package jetbrains.buildServer.sesPlugin.sqs
 
-import com.google.gson.Gson
+import com.google.gson.*
+import java.lang.reflect.Type
 
 class SQSNotificationParserImpl : SQSNotificationParser {
-    private val gson = Gson()
+    private val gson: Gson
+
+    init {
+        val gsonBuilder = GsonBuilder()
+        gsonBuilder.registerTypeAdapter(AmazonSQSNotification::class.java, AmazonSQSNotificationDeserializer())
+        gson = gsonBuilder.create()
+    }
 
     override fun parse(data: String): AmazonSQSNotificationParseResult {
         var res: AmazonSQSNotification?
@@ -21,12 +28,32 @@ class SQSNotificationParserImpl : SQSNotificationParser {
 
     @Suppress("SENSELESS_COMPARISON") // GSON
     private fun validate(res: AmazonSQSNotification, data: String): SQSNotificationParseException? {
-        if (res.message == null) {
+        if (res.Message == null) {
             return SQSNotificationParseException("Cannot parse data: message should not be empty ($data)")
         }
-        if (res.type == null) {
+        if (res.Type == null) {
             return SQSNotificationParseException("Cannot parse data: type should not be empty ($data)")
         }
         return null
+    }
+
+    inner class AmazonSQSNotificationDeserializer : JsonDeserializer<AmazonSQSNotification> {
+        override fun deserialize(element: JsonElement, type: Type, context: JsonDeserializationContext): AmazonSQSNotification {
+            val obj = element.asJsonObject
+
+            val Type = obj["Type"]?.asString ?: throw JsonParseException("Type not found")
+            val MessageId = obj["MessageId"]?.asString ?: throw JsonParseException("MessageId not found")
+            val TopicArn = obj["TopicArn"]?.asString ?: throw JsonParseException("TopicArn not found")
+            val Subject = obj["Subject"]?.asString ?: throw JsonParseException("Subject not found")
+            val Message = if (obj["Message"] != null) gson.fromJson<JsonObject>(obj["Message"].asString, JsonObject::class.java) else throw JsonParseException("Message not found")
+            val Timestamp = obj["Timestamp"].asString ?: throw JsonParseException("Timestamp not found")
+            val SignatureVersion = obj["SignatureVersion"].asString ?: throw JsonParseException("SignatureVersion not found")
+            val Signature = obj["Signature"].asString ?: throw JsonParseException("Signature not found")
+            val SigningCertURL = obj["SigningCertURL"].asString ?: throw JsonParseException("SigningCertURL not found")
+            val UnsubscribeURL = obj["UnsubscribeURL"]?.asString
+
+            return AmazonSQSNotification(Type, MessageId, TopicArn, Subject, Message, Timestamp, SignatureVersion, Signature, SigningCertURL, UnsubscribeURL)
+        }
+
     }
 }
