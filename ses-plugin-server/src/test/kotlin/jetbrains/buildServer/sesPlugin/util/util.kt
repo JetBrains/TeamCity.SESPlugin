f71cc302d9e1f5bedb9f23e57d45757f416fe5fa
linfar <andrey.titov@jetbrains.com>
2017-12-13 15:37:58 +0300

    API to retrieve test resources

--- ses-plugin-server/src/test/kotlin/jetbrains/buildServer/sesPlugin/util/util.kt
+++ ses-plugin-server/src/test/kotlin/jetbrains/buildServer/sesPlugin/util/util.kt
@@ -2,6 +2,9 @@ package jetbrains.buildServer.sesPlugin.util
 
 import org.jmock.Expectations
 import org.jmock.Mockery
+import java.io.IOException
+import java.io.InputStream
+import java.nio.charset.Charset
 import kotlin.reflect.KClass
 
 fun Mockery.check(action: Expectations.() -> Unit) {
@@ -21,4 +24,15 @@ fun mocking(action: Mockery.() -> Unit) {
 
 fun <T : Any> Mockery.mock(clazz: KClass<T>, name: String? = null): T {
     return this.mock(clazz.java, name)
+}
+
+fun Any.getResource(name: String): TestResource {
+    val stream = ClassLoader.getSystemResourceAsStream(this::class.qualifiedName?.replace('.', '/') + "_" + name) ?: throw IOException(this::class.qualifiedName + "_" + name + " not found")
+    return TestResource(stream)
+}
+
+class TestResource(private val stream: InputStream) {
+    fun asString(): String {
+        return stream.readBytes().toString(Charset.forName("UTF-8"))
+    }
 }
