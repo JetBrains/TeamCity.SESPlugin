7262712b03a5c3d34a2938b277375a4a69c98c2a
linfar <andrey.titov@jetbrains.com>
2017-12-13 17:20:13 +0300

    Test for RescanSQSTaskImpl

---
+++ ses-plugin-server/src/test/kotlin/jetbrains/buildServer/sesPlugin/sqs/RescanSQSTaskImplTest.kt
@@ -0,0 +1,27 @@
+package jetbrains.buildServer.sesPlugin.sqs
+
+import jetbrains.buildServer.sesPlugin.teamcity.SESIntegrationManager
+import jetbrains.buildServer.sesPlugin.teamcity.SQSBean
+import jetbrains.buildServer.sesPlugin.util.check
+import jetbrains.buildServer.sesPlugin.util.mock
+import jetbrains.buildServer.sesPlugin.util.mocking
+import org.jmock.Expectations
+import org.testng.annotations.Test
+
+class RescanSQSTaskImplTest {
+    @Test
+    fun test() {
+        mocking {
+            val reader = mock(SQSMessagesReader::class)
+            val manager = mock(SESIntegrationManager::class)
+            val list = listOf(mock(SQSBean::class)).asSequence()
+
+            check {
+                one(reader).readAllQueues(list)
+                one(manager).getBeans(""); will(Expectations.returnValue(list))
+            }
+
+            RescanSQSTaskImpl(reader, manager).run()
+        }
+    }
+}
