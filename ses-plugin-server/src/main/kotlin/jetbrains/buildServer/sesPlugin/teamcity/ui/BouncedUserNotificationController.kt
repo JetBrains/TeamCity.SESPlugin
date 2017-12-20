4e790a70f2c9121674607684e190643ca14ea9c7
linfar <andrey.titov@jetbrains.com>
2017-12-20 15:01:17 +0300

    Show configurable responsible contact in the user bounce notification

--- ses-plugin-server/src/main/kotlin/jetbrains/buildServer/sesPlugin/teamcity/ui/BouncedUserNotificationController.kt
+++ ses-plugin-server/src/main/kotlin/jetbrains/buildServer/sesPlugin/teamcity/ui/BouncedUserNotificationController.kt
@@ -1,5 +1,6 @@
 package jetbrains.buildServer.sesPlugin.teamcity.ui
 
+import jetbrains.buildServer.server.responsible.ResponsibleContactProvider
 import jetbrains.buildServer.sesPlugin.email.emailDisableDescription
 import jetbrains.buildServer.sesPlugin.email.emailDisabled
 import jetbrains.buildServer.users.SUser
@@ -12,13 +13,21 @@ import javax.servlet.http.HttpServletRequest
 
 
 class BouncedUserNotificationController(pagePlaces: PagePlaces,
-                                        pluginDescriptor: PluginDescriptor)
+                                        pluginDescriptor: PluginDescriptor,
+                                        private val responsibleContact: ResponsibleContactProvider)
     : SimplePageExtension(pagePlaces, PlaceId.ALL_PAGES_HEADER, "BouncedUserNotificationController", pluginDescriptor.getPluginResourcesPath("bouncedUserNotification.jsp")) {
 
     override fun isAvailable(request: HttpServletRequest): Boolean = request.user()?.emailDisabled ?: false
 
     override fun fillModel(model: MutableMap<String, Any>, request: HttpServletRequest) {
         model.put("disableDescription", request.user()!!.emailDisableDescription)
+
+        val globalResponsible = responsibleContact.getGlobal()
+        if (globalResponsible.isPresent) {
+            model.put("contactInfo", globalResponsible.get())
+        } else {
+            model.put("contactInfo", "Contact your TeamCity administrator for details")
+        }
     }
 }
 
--- ses-plugin-server/src/main/resources/buildServerResources/bouncedUserNotification.jsp
+++ ses-plugin-server/src/main/resources/buildServerResources/bouncedUserNotification.jsp
@@ -1,5 +1,6 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jstl/core" %><%--@elvariable id="contactInfo" type="java.lang.String"--%>
 <script>
     $j(function () {
-        $j('#globalHealthItems').append('<div class=\'icon_before icon16 attentionComment clearfix\'><div class=\'global-health-item__content\'>Your email was disabled</div></div>');
+        $j('#globalHealthItems').append('<div class=\'icon_before icon16 attentionComment clearfix\'><div class=\'global-health-item__content\'>Your email was disabled. <c:out value="${contactInfo}"/>. </div></div>');
     });
 </script>
