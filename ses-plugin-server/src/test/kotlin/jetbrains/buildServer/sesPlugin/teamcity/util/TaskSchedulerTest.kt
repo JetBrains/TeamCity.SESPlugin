32b514f9c0530818ce4f171bc5e1ff06bd08dab9
linfar <andrey.titov@jetbrains.com>
2018-06-21 17:17:03 +0300

    Add test

--- ses-plugin-server/src/test/kotlin/jetbrains/buildServer/sesPlugin/teamcity/util/TaskSchedulerTest.kt
+++ ses-plugin-server/src/test/kotlin/jetbrains/buildServer/sesPlugin/teamcity/util/TaskSchedulerTest.kt
@@ -6,10 +6,12 @@ import jetbrains.buildServer.sesPlugin.util.check
 import jetbrains.buildServer.sesPlugin.util.mock
 import jetbrains.buildServer.sesPlugin.util.mocking
 import jetbrains.buildServer.util.EventDispatcher
-import org.jmock.Expectations.any
-import org.jmock.Expectations.returnValue
+import org.jmock.Expectations.*
+import org.jmock.api.Invocation
+import org.jmock.lib.action.CustomAction
 import org.testng.annotations.Test
 import java.util.concurrent.ScheduledExecutorService
+import java.util.concurrent.ScheduledFuture
 import java.util.concurrent.TimeUnit
 
 class TaskSchedulerTest {
@@ -54,6 +56,42 @@ class TaskSchedulerTest {
 
             startupProcessor.init()
             dispatcher.multicaster.serverStartup()
+        }
+    }
+
+    @Test
+    fun testErrorOccurred() {
+        mocking {
+            val executorServices = mock(ExecutorServices::class)
+            val executor = mock(ScheduledExecutorService::class)
+            val task = mock(PeriodicTask::class)
+            val taskRunnableMock = mock(Runnable::class)
+            val taskRunnable: () -> Unit = { taskRunnableMock.run() }
+            val dispatcher = EventDispatcher.create(ServerListener::class.java)
+            val future = mock(ScheduledFuture::class)
+
+            val startupProcessor = TaskScheduler(executorServices, dispatcher, listOf(task))
+
+            check {
+                one(executorServices).normalExecutorService; will(returnValue(executor))
+
+                one(task).task; will(returnValue(taskRunnable))
+                one(task).initialDelay; will(returnValue(10L))
+                one(task).delay; will(returnValue(11L))
+                one(taskRunnableMock).run(); will(throwException(RuntimeException()))
+
+                one(executor).scheduleAtFixedRate(with(any(Runnable::class.java)), with(10L), with(11L), with(TimeUnit.MILLISECONDS));
+                will(object : CustomAction("") {
+                    override fun invoke(p0: Invocation?): Any {
+                        (p0!!.getParameter(0) as Runnable).run()
+                        return future
+                    }
+
+                })
+            }
+
+            startupProcessor.init()
+            startupProcessor.serverStartup()
         }
     }
 }
