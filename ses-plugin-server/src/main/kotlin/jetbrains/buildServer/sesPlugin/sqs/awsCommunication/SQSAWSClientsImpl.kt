3645ab3b49adc68f0d31117c954495985392ea37
linfar <andrey.titov@jetbrains.com>
2018-08-24 17:15:45 +0300

    Incorrect IllegalArgumentException

--- ses-plugin-server/src/main/kotlin/jetbrains/buildServer/sesPlugin/sqs/awsCommunication/SQSAWSClientsImpl.kt
+++ ses-plugin-server/src/main/kotlin/jetbrains/buildServer/sesPlugin/sqs/awsCommunication/SQSAWSClientsImpl.kt
@@ -4,11 +4,6 @@ import com.amazonaws.auth.AWSCredentials
 import jetbrains.buildServer.util.amazon.AWSClients
 
 class SQSAWSClientsImpl(private val realClients: AWSClients) : SQSAWSClients {
-
-    init {
-        if (realClients.credentials == null) throw IllegalArgumentException("Cannot obtain credentials")
-    }
-
     override val credentials: AWSCredentials?
         get() = realClients.credentials
     override val region: String
